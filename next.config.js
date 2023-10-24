bd6eb558594d0ac3e6fc6c27a7c9eb46c6fe1dfc
T0jgli <kovalikadam00@gmail.com>
2023-10-24 20:18:26 +0200

    headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
+const headers = [
+    {
+        key: "Referrer-Policy",
+        value: "no-referrer, strict-origin-when-cross-origin",
+    },
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains",
+    },
+    {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+    },
+    {
+        key: "X-Frame-Options",
+        value: "DENY",
+    },
+    {
+        key: "X-XSS-Protection",
+        value: "nosniff",
+    },
+    {
+        key: "Permissions-Policy",
+        value: "geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()",
+    },
+];
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 });
@@ -16,32 +43,11 @@ module.exports = withBundleAnalyzer({
             {
                 // Sets security headers for all routes
                 source: "/(.*)",
-                headers: [
-                    {
-                        key: "Referrer-Policy",
-                        value: "no-referrer, strict-origin-when-cross-origin",
-                    },
-                    {
-                        key: "Strict-Transport-Security",
-                        value: "max-age=63072000; includeSubDomains",
-                    },
-                    {
-                        key: "X-Content-Type-Options",
-                        value: "nosniff",
-                    },
-                    {
-                        key: "X-Frame-Options",
-                        value: "DENY",
-                    },
-                    {
-                        key: "X-XSS-Protection",
-                        value: "nosniff",
-                    },
-                    {
-                        key: "Permissions-Policy",
-                        value: "geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()",
-                    },
-                ],
+                headers: headers,
+            },
+            {
+                source: "/",
+                headers: headers,
             },
         ];
     },
